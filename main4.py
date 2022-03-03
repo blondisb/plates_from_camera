0e87ccefcbe7f91cbf78eeb9f0486243cfa4ba40
blondisb <blondisb@uninorte.edu.co>
2022-03-03 13:41:28 -0500

    final

--- main4.py
+++ main4.py
@@ -1,23 +1,28 @@
 import cv2
 import numpy as np
 from PIL import Image
+from pytesseract import pytesseract
 import database as dtb
 import reg_exp as rxp
-from pytesseract import pytesseract
 from datetime import datetime, date, time, timedelta
 
-
+video = 'rtsp://root:admin@169.254.57.130/axis-media/media.amp?resolution=1280x720&fps=25&videocodec=h264'
 texto=""
+
 def tesseract(imagen):
     # path_to_tesseract = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
     path_to_tesseract = r'D:\Programs\Tesseract\tesseract.exe'    
+
     image_path = 'test.jpg'
     pytesseract.tesseract_cmd = path_to_tesseract
     text = pytesseract.image_to_string(imagen,config='--psm 11')
     print(text[:-1])
     return text[:-1]
 
-cap = cv2.VideoCapture(1)
+# cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(video)
+
+
 amarilloBajo = np.array([15,100,20],np.uint8)
 amarilloAlto = np.array([45,255,255],np.uint8)
 
@@ -49,19 +54,17 @@ while True:
         #print(len(approx))
         x,y,w,h = cv2.boundingRect(approx)
         
-        if len(approx)==4 and area > 5000:
+        if len(approx)==4 and area > 3000:
           aspect_ratio = float(w)/h
           if aspect_ratio > 2.1:
               print('aspect_ratio= ', aspect_ratio)
               placa = gray[y-51:y+h+51,x-51:x+w+51]
-              texto = tesseract( cv2.blur(placa,(3,3)))
+              texto = tesseract(placa)
 
-              texto = rxp.platesformat(texto)
               now = date.today()
               nowdate = now.strftime('%d/%m/%Y')
               dtb.insertrow(texto,nowdate)
 
-            #   dtb.exportcv()
               cv2.imwrite('test.png',placa)
             
               cv2.imshow('PLACA',placa)
@@ -71,15 +74,13 @@ while True:
 
 
     cv2.imshow("gray",frame)
-    
-    
+       
     #cv2.imwrite('test.jpg',  canny  )
     
     if cv2.waitKey(1) & 0xFF == ord('s'):
+        dtb.exportcv()
         break
 cap.release()
 cv2.destroyAllWindows()
-
-
 
 
--- reg_exp.py
+++ reg_exp.py
@@ -3,13 +3,13 @@ import re
 
 def platesformat(txt):
     # stuff = re.findall('^[A-Z]{3}\s[0-9]{2}[A-Z0-9]',txt)
-    stuff = re.findall('^[A-Z]{3}.[0-9]{2}[A-Z0-9]',txt)
+    stuff = re.findall('^[A-Z]{3}[-][0-9]{2}[A-Z0-9]',txt)
     # if len(stuff) !=1: continue       # Malo
     for i in stuff:
         plate = str(stuff[0])
         # platelist.append(plate)       # Malo
         # print(platelist)              # Malo
-        print(plate, "reg-exp")
+        # print(plate, "reg-exp")
         return(plate)
 
 # platesformat("a")
@@ -18,3 +18,4 @@ def platesformat(txt):
 # platesformat("AAA 123")
 # platesformat("AAA 12A")
 # platesformat("AAAA12A")
+# platesformat("AAA-12A")
